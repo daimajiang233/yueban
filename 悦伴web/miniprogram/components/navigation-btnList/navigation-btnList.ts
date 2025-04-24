f4c0c5ee1dd10035da069c896c9de66ba4da09ab
daimajiang233 <2116557388@qq.com>
2025-04-24 22:22:53 +0800

    更新首页

---
+++ 悦伴web/miniprogram/components/navigation-btnList/navigation-btnList.ts
@@ -0,0 +1,22 @@
+Component({
+  properties: {
+    zhTitle: {
+      type: String,
+      value: ''
+    },
+    enTitle: {
+      type: String,
+      value: ''
+    },
+    iconUrl: {
+      type: String,
+      value: ''
+    }
+  },
+  data: {
+    // 组件内部数据
+  },
+  methods: {
+    // 组件方法
+  }
+})
