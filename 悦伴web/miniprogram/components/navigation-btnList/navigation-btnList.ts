97e602ffdd2de3a1dcd0ee37acfcac296a02ebdb
daimajiang233 <2116557388@qq.com>
2025-05-08 16:43:47 +0800

    update

--- 悦伴web/miniprogram/components/navigation-btnList/navigation-btnList.ts
+++ 悦伴web/miniprogram/components/navigation-btnList/navigation-btnList.ts
@@ -32,7 +32,24 @@ Component({
             // 判断蓝牙已连接 发送指令
             
             console.log(state.status);
-           
+            const buffer = new ArrayBuffer(2);
+            const dataView = new DataView(buffer);
+            dataView.setUint16(0, state.status, false); // 发送 指令 作为 16 位值，大端序
+            wx.writeBLECharacteristicValue({
+                deviceId: app.globalData.deviceId,
+                serviceId: app.globalData.serviceId,
+                characteristicId: app.globalData.characteristicId,
+                value: buffer,
+                success: () => {
+                    this.setData({ status: ' distinguir指令发送成功' });
+                    console.log('指令发送成功');
+                    // this.enableNotifications(); // 发送指令后启用通知
+                },
+                fail: (res) => {
+                    this.setData({ status: `指令发送失败: ${res.errMsg}` });
+                }
+            });
+
         }else{
             wx.showToast({
                 title: "蓝牙未连接",
