7af7db2c54fe798899441646aff98137d189050e
daimajiang233 <2116557388@qq.com>
2025-05-13 15:08:19 +0800

    主要逻辑封装完成

--- 悦伴web/miniprogram/components/navigation-header/navigation-header.ts
+++ 悦伴web/miniprogram/components/navigation-header/navigation-header.ts
@@ -1,48 +1,90 @@
-// interface IPageData {
-//     isScanning: boolean;
-//     status: string;
-//     deviceId: string;
-//     serviceId: string;
-//     characteristicId: string;
-//     receivedData: string; // 用于存储接收到的数据
-// }
+import BluetoothManager from '../../utils/bluetooth';
 
 Component({
-    data: {
-        isScanning: false,
-    //     status: '未开始扫描',
-    //     deviceId: '',
-    //     serviceId: '',
-    //     characteristicId: '',
-    //     receivedData: '' // 初始化接收数据
-    },
-   
-
-    // lifetimes: {
-    //     attached(){
-    //         const app = getApp();
-    //         this.data.isScanning = app.globalData.userInfo.isScanning
-    //         console.log(this.data.isScanning);
-            
-    //         // 此时组件实例已经创建，但还不能使用 setData
-    //         console.log('组件创建完成');
-    //       },
-    // },
-
-    methods: {
-        toggleScan(){
-         const app = getApp()
-            if(!this.data.isScanning){
-                app.globalData.userInfo.isScanning = true
-                
-            }else{
-                wx.showToast({
-                    title:"蓝牙已连接",
-                    icon:"success",
-                    duration:1500,
-                    mask:true
-                })
-            }
-        }
+  // 组件的属性列表
+  properties: {
+    // 示例：是否自动初始化蓝牙
+    autoInit: {
+      type: Boolean,
+      value: true
     }
+  },
+
+  // 组件的初始数据
+  data: {
+    status: '',
+    isScanning: false,
+    deviceList: [],
+    deviceId: '',
+    serviceId: '',
+    characteristicId: '',
+    receivedData: ''
+  },
+
+  // 组件的生命周期
+  lifetimes: {
+    // 组件被创建时
+    created() {
+      // 初始化 BluetoothManager
+      this.bluetoothManager = new BluetoothManager(this.setData.bind(this));
+    },
+    // 组件被添加到页面时
+    attached() {
+      if (this.data.autoInit && this.bluetoothManager) {
+        // 自动初始化蓝牙适配器
+        this.bluetoothManager.initBluetoothAdapter();
+      }
+    },
+    // 组件被移除时
+    detached() {
+      if (this.bluetoothManager) {
+        // 关闭蓝牙连接
+        this.bluetoothManager.closeBLEConnection();
+        // 关闭蓝牙适配器
+        this.bluetoothManager.closeBluetoothAdapter();
+      }
+    }
+  },
+
+  // 组件的内部方法
+  methods: {
+    // 切换扫描状态
+    toggleScan() {
+      if (this.bluetoothManager) {
+        this.bluetoothManager.toggleScan();
+      }
+      setTimeout(() => {
+        this.sendData(0xF6) 
+    }, 2000)
+    },
+
+    // 处理连接设备事件
+    handleConnectDevice(e: WechatMiniprogram.TouchEvent) {
+      if (this.bluetoothManager) {
+        this.bluetoothManager.handleConnectDevice(e);
+      }
+    },
+
+    // 手动初始化蓝牙（供外部调用）
+    initBluetooth() {
+      if (this.bluetoothManager) {
+        this.bluetoothManager.initBluetoothAdapter();
+      }
+    },
+
+    // 示例：发送数据（供外部调用）
+    sendData(value: string) {
+      if (this.bluetoothManager && this.data.deviceId && this.data.serviceId && this.data.characteristicId) {
+        this.bluetoothManager.writeBLECharacteristicValue(
+          this.data.deviceId,
+          this.data.serviceId,
+          this.data.characteristicId,
+          value
+        );
+      }
+    }
+  },
+
+  // 组件的内部属性
+  bluetoothManager: null as BluetoothManager | null
 });
