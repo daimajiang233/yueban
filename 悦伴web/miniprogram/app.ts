fdf02a89a27e583ec0f7fae65e45027c1e889812
daimajiang233 <2116557388@qq.com>
2025-05-23 14:46:12 +0800

    创建云函数初始化

--- 悦伴web/miniprogram/app.ts
+++ 悦伴web/miniprogram/app.ts
@@ -4,7 +4,7 @@
 App<IAppOption>({
   globalData: {
     userInfo:{
-        name: "AW31N_1YUE BAN",
+        name: "YUE BAN",
         status: false,
         isScanning: false,
         deviceId: "",
@@ -20,6 +20,14 @@ App<IAppOption>({
     this.globalData.userInfo = userInfo;
   },
   onLaunch() {
+    // 初始化晕哈数
+    if (!wx.cloud) {
+        console.error('请使用支持云能力的微信小程序版本');
+        return;
+      }
+      wx.cloud.init({
+        env: wx.cloud.DYNAMIC_CURRENT_ENV, // 替换为实际环境ID，或使用 cloud.DYNAMIC_CURRENT_ENV
+      })
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
--- 悦伴web/miniprogram/pages/share/share.ts
+++ 悦伴web/miniprogram/pages/share/share.ts
@@ -8,11 +8,24 @@ Page({
 
   },
 
+  dataFn(){
+    wx.cloud.callFunction({
+        name: 'remote', // 替换为实际的云函数名称
+        data: { /* 可以在这里传入需要的参数 */name: 'test' },
+        success: res => {
+          console.log(res.result); // 输出返回的结果
+        },
+        fail: err => {
+          console.error(err); // 调用失败的回调
+        }
+      });
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
-
+    this.dataFn()
   },
 
   /**
